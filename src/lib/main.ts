bef44b6f429fc08a35db5265c10bc7358c81f34c
Veera <nmani0303@gmail.com>
2024-01-07 12:25:04 +0800

    support multiple yml

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -10,17 +10,24 @@ import {
 import Arranger from '../core/arranger';
 import Actor from '../core/actor';
 import Asserter from '../core/asserter';
+import { glob } from 'glob';
+import { Test } from '../core/types';
 
 async function main() {
-
-    const file = fs.readFileSync('src/tests/login-tests.yml', 'utf8')
-    let { tests } = await yaml.parse(file);
+    const paths = await glob('**/tests/**/*.{yaml,yml}');
+    let allTests: Test[] = [];
+    for (let path of paths) {
+        let file = fs.readFileSync(path, 'utf8');
+        let suite: { tests: Test[] } = await yaml.parse(file);
+        let { tests } = suite;
+        allTests.push(...tests)
+    }
     let browser: Browser = await chromium.launch({ headless: false });
-    for (let test of tests) {
+    for (let test of allTests) {
         console.log(`Starting the test: ${test.name}`)
         let context: BrowserContext = await browser.newContext(devices['Desktop Chrome']);
         let pwPage: Page = await context.newPage();
-        let arranger: Arranger = new Arranger(pwPage, test.arrange);
+        let arranger = new Arranger(pwPage, test.arrange);
         let actor = new Actor(pwPage, test.act);
         let asserter = new Asserter(pwPage, test.assert);
         await arranger.arrange();
