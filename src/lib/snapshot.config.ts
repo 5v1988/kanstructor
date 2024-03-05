76e4f8d24b6a0a95fa3dd43a116191a96d23f4d5
Veera <nmani0303@gmail.com>
2024-03-05 22:59:07 +0800

    fix snapshot config

--- src/lib/snapshot.config.ts
+++ src/lib/snapshot.config.ts
@@ -1,14 +1,23 @@
-export const snapshotOptions = {
+import { ComparisonOptions } from "resemblejs";
+
+export const snapshotOptions: ComparisonOptions = {
     output: {
         errorColor: {
             red: 255,
             green: 0,
             blue: 255
         },
+        errorType: "flat",
         transparency: 0.3,
         largeImageThreshold: 1200,
         useCrossOrigin: false,
-        outputDiff: true
+        boundingBox: {
+            left: 100,
+            top: 100,
+            right: 400,
+            bottom: 300
+        }
     },
-    scaleToSameSize: true
+    scaleToSameSize: true,
+    ignore: "antialiasing"
 };
